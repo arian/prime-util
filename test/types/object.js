427217b4dd1c63b7c66ba4bf66d56bef6c102a71
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-08-02 13:18:27 +0200

    Initial Commit, Adding Object.merge

---
+++ test/types/object.js
@@ -0,0 +1,21 @@
+"use strict"
+
+var expect = require("expect.js")
+var object = require("../../types/object")
+
+describe('merge', function(){
+
+    it('should merge any object inside the passed in object, and should return the passed in object', function(){
+        var a = {a:1, b:2, c: {a:1, b:2, c:3}}
+        var b = {c: {d:4}, d:4}
+        var c = {a: 5, c: {a:5}}
+
+        var merger = object.merge(a, b);
+
+        expect(merger).to.eql({a:1, b:2, c:{a:1, b:2, c:3, d:4}, d:4})
+        expect(merger).to.be(a)
+
+        expect(object.merge(a, b, c)).to.eql({a:5, b:2, c:{a:5, b:2, c:3, d:4}, d:4})
+    })
+
+});
---
+++ types/object.js
@@ -0,0 +1,23 @@
+/*
+object methods
+*/"use strict"
+
+var shell = require("prime/util/shell")
+var type = require("prime/util/type")
+
+var object = shell({
+
+    merge: function(key, value){
+        if (typeof key == 'string'){
+            if (type(this[key]) == 'object' && type(value[key] == 'object')) object.merge(this[key], value)
+            else this[key] = value
+        } else for (var i = 0; i < arguments.length; i++){
+            var obj = arguments[i]
+            for (var k in obj) object.merge(this, k, obj[k])
+        }
+        return this
+    }
+
+})
+
+module.exports = object
