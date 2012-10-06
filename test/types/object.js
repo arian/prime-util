e3e6c6b79046f947ee2068ebd606a2cd6b630b5e
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-10-06 14:20:42 +0200

    Wrap the object.merge tests in another describe()

--- test/types/object.js
+++ test/types/object.js
@@ -1,21 +1,25 @@
-"use strict"
+"use strict";
 
 var expect = require("expect.js")
 var object = require("../../types/object")
 
-describe('merge', function(){
+describe("Object", function(){
 
-    it('should merge any object inside the passed in object, and should return the passed in object', function(){
-        var a = {a:1, b:2, c: {a:1, b:2, c:3}}
-        var b = {c: {d:4}, d:4}
-        var c = {a: 5, c: {a:5}}
+    describe('merge', function(){
 
-        var merger = object.merge(a, b);
+        it('should merge any object inside the passed in object, and should return the passed in object', function(){
+            var a = {a:1, b:2, c: {a:1, b:2, c:3}}
+            var b = {c: {d:4}, d:4}
+            var c = {a: 5, c: {a:5}}
 
-        expect(merger).to.eql({a:1, b:2, c:{a:1, b:2, c:3, d:4}, d:4})
-        expect(merger).to.be(a)
+            var merger = object.merge(a, b)
 
-        expect(object.merge(a, b, c)).to.eql({a:5, b:2, c:{a:5, b:2, c:3, d:4}, d:4})
+            expect(merger).to.eql({a:1, b:2, c:{a:1, b:2, c:3, d:4}, d:4})
+            expect(merger).to.be(a)
+
+            expect(object.merge(a, b, c)).to.eql({a:5, b:2, c:{a:5, b:2, c:3, d:4}, d:4})
+        })
+
     })
 
-});
+})
