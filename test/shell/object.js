d973c5ad940110c26dce591ff8881b23a0265245
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-02-11 20:42:17 +0100

    whitespace

--- test/shell/object.js
+++ test/shell/object.js
@@ -22,22 +22,21 @@ describe("Object", function(){
 
     })
 
-    describe('fromPath', function() {
+    describe('fromPath', function(){
 
-        it('should return the value at a given path. Path should be passed as dot notation "child.parent.name"', function() {
+        it('should return the value at a given path. Path should be passed as dot notation "child.parent.name"', function(){
             var a = {'a': {'b': {'c': 'prime!'}}}
             var value = object.fromPath(a, 'a.b.c')
             expect(value).to.eql('prime!')
         })
 
-        it('don\'t change anything at the main obj', function() {
+        it('don\'t change anything at the main obj', function(){
             var a = {'a': {'b': {'c': 'prime!'}}}
             var value = object.fromPath(a, 'a.b.c')
-
             expect(a).to.eql({'a': {'b': {'c': 'prime!'}}})
         })
 
-        it('should return "null" if path doesn\'t exist', function() {
+        it('should return "null" if path doesn\'t exist', function(){
             var a = {'a': {'b': {'c': 'prime!'}}}
             var value = object.fromPath(a, 'a.b.d')
             expect(value).to.be(null)
